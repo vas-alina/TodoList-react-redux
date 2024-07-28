7a04357db76a43ec5ce5625531603666cfc3169a
vas-alina <vasalina93@gmail.com>
2024-07-28 14:36:16 +0300

    Добавлен ReduxDevTools

--- src/store.js
+++ src/store.js
@@ -1,5 +1,10 @@
-import { applyMiddleware, createStore } from "redux";
+import { applyMiddleware, createStore, compose } from "redux";
 import rootReducer from "./reducers";
 import { thunk } from "redux-thunk";
 
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
